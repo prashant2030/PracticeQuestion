64f88424c5877b1dd2d953abbc1e80ab073a3ffd
prashant2030 <126760712+prashant2030@users.noreply.github.com>
2023-09-06 22:43:21 +0530

    Add files via upload

---
+++ StaticKeyWord.java
@@ -0,0 +1,15 @@
+
+public class StaticKeyWord {
+
+    static int age;
+      static{
+      age=18;
+      System.out.println("static block");
+    
+
+    }
+    public static void main(String[] args) {
+        System.out.println("main method");
+    }
+    
+}
